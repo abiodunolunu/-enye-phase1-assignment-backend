0a44ef3d3dcf3a1675150d5a9cc3e7c41004db33
abeydev <abiodunolunu@gmail.com>
2021-01-20 21:23:41 +0100

    added project's live link

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
 # This is the backend task for enye
+
+[Project Live Link](https://abeydev-enyebackend-task.herokuapp.com/api/rates?base=USD&currency=JPY,EUR,CZK)
--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const routes = require('./routes/index.js')
 app.use(bodyParser.json())
 app.use('/api', routes)
 
-
-
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`)
